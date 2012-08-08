f05552ed6f0214fdfe75817e784991272cc69a8e
Régis Mesquita <regis.mesquita@codeminer42.com>
2012-08-08 06:26:06 -0300

    Refactoring the code so it get more readable... :P

--- legenda_downloader.rb
+++ legenda_downloader.rb
@@ -6,12 +6,11 @@ USUARIO = 'user'
 SENHA = 'senha'
 # Uso : ruby legenda_downloader.rb "The Matrix"
 
+require 'scanf'
 require 'net/http'
 require 'uri'
 
 class LegendaDownloader
-
-
   def initialize
     if File.exists?('ltv-account.cfg')
       File.open('ltv-account.cfg').read.each_line do |line|
@@ -24,6 +23,7 @@ class LegendaDownloader
       @senha = SENHA
     end
   end
+
   def login
     if $standalone then puts 'Logando no site' end
     url = URI.parse('http://legendas.tv/login_verificar.php')
@@ -38,7 +38,8 @@ class LegendaDownloader
       res.error!
     end
   end
-  def buscar(searchParameters)
+
+  def search_movie(searchParameters)
     if $standalone then puts 'Buscando no site' end
     url = URI.parse('http://legendas.tv/index.php?opcao=buscarlegenda')
     req = Net::HTTP::Post.new('/index.php?opcao=buscarlegenda',@headers)
@@ -47,23 +48,20 @@ class LegendaDownloader
     res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
     case res
     when Net::HTTPSuccess, Net::HTTPRedirection
-      lista_de_filmes = Hash.new
+      lista_de_movies = Array.new
       res.body.each_line do |line|
         if line.match(/abredown/) then
           nome_do_release = line.split(',')[2]
           id_para_download = line.split('abredown(')[1].split('\'')[1]
-          lista_de_filmes[nome_do_release] = id_para_download
+          lista_de_movies.push({:release_name => nome_do_release , :download_hash => id_para_download}) unless id_para_download.nil?
         end
       end
-      lista_de_filmes.delete(nil)
-      lista_de_filmes= (1..lista_de_filmes.size).zip(lista_de_filmes)
-      lista_de_filmes = Hash[lista_de_filmes]
-
+      return lista_de_movies
     else
       raise res.error!
     end
-
   end
+
   def get_download_link(hash)
     if $standalone then puts "Legenda sendo baixada" end
     path= "/info.php?d=#{hash}&c=1"
@@ -98,39 +96,41 @@ class LegendaDownloader
     else
       raise res.error!
     end
-
-
   end
-  def self.legenda_for(filme)
-    #Pega a primeira legenda encontrada e baixa =P I'm Feeling Lucky.
-    baixador = LegendaDownloader.new
-    baixador.login
-    filmes = baixador.buscar(filme)
-    hash_do_filme = filme[1]
-    if filmes.size == 0
-      puts filmes.inspect if ENV['DEBUG']
+
+  def prompt_for_movie_select(movies)
+      puts "Filmes encontrados"
+      movies_list = Hash[(1..movies.size).zip(movies)]
+      movies_list.sort{|x,y|x[0]<=>y[0]}.each{|id,movie| puts "#{id} - #{movie[:release_name]}"}
+      choices = nil
+      begin
+          print "Escolha que legenda baixar: "
+          choices = STDIN.gets.split(" ").map{|x| x.to_i}
+          choices.delete(0)
+          choice = choices.first
+      end while choice.class != Fixnum
+      exit if choices.first == -1
+      return choices.map{|choice| movies_list[choice]}
+  end
+
+  def select_movie(movies)
+    if movies.size == 0
+      puts movies.inspect if ENV['DEBUG']
       puts "Nenhum filme encontrado."
       exit
+    elsif movies.size == 1
+      return movies
     else
-      puts "Filmes encontrados"
-      filmes.sort{|x,y|x[0]<=>y[0]}.each{|id,filme| puts "#{id} - #{filme[0]}"}
-      if filmes.size == 1
-          escolhas = [1]
-      else
-          require 'scanf'
-          escolhas = nil
-          begin
-              print "Escolha que legenda baixar: "
-              escolhas = STDIN.gets.split(" ").map{|x| x.to_i}
-              escolhas.delete(0)
-              escolha = escolhas.first
-          end while escolha.class != Fixnum
-          exit if escolhas.first == -1
-      end
-      escolhas.each do |escolha|
-          hash_do_filme = filmes[escolha][1]
-          baixador.download(hash_do_filme)
-      end
+      return prompt_for_movie_select(movies)
+    end
+  end
+
+  def self.legenda_for(filme)
+    downloader = LegendaDownloader.new
+    downloader.login
+    movies = downloader.search_movie(filme)
+    downloader.select_movie(movies).each do |movie|
+      downloader.download(movie[:download_hash])
     end
   end
 end
